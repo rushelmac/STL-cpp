c53895fcd4b3f31c14920d4f4beccfffb4aac3a9
Rushikesh Shelke <58942280+rushelmac@users.noreply.github.com>
2020-01-07 17:15:40 +0530

    Queue using list.h
    This includes using list.h already created.

---
+++ queue_list.h
@@ -0,0 +1,40 @@
+#include<iostream>
+#include"nisha_list.h"
+using namespace std;
+
+template<typename T> 
+class queue
+{
+	list<T> li;
+	int size;
+	public:
+		queue();
+		void push(T);
+		T pop();
+		T* front();
+		T* back();
+		
+};
+
+template<typename T>
+queue<T>::queue()
+{
+	size=0;
+}
+template<typename T>
+void queue<T>::push(T d)
+{
+	li.push_back(d);
+	size++;	
+}
+
+template<typename T>
+T queue<T>::pop()
+{
+	if(size!=0)
+	{
+		size--;
+		return li.pop_back();
+	}
+	
+}
